c949f38f3566cf42787456d1e7a9f01102e5dd0e
CondingBot000 <codepipe@naver.com>
2021-07-10 15:51:17 +0900

    init

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log('aaaa')
+
+function testCall() {
+    // alert("I'm testCall")
+    makeH1Html("I am Simple TestCall")
+
+    
+}
+
+function returnValueTest() {
+    let returnValue = "I'm return Value from Javascript Func" 
+    makeH1Html(returnValue)
+    return returnValue
+}
+
+function parameterTest(paramValue) {
+    // alert(paramValue)
+    makeH1Html("parameterTest => " + paramValue)
+}
+
+function callNativeFunc() {
+    window.webkit.messageHandlers.test.postMessage("callNativeFunc param")
+
+}
+
+function makeH1Html(text) {
+    var h = document.createElement("H3");
+    var t = document.createTextNode(text);
+    h.appendChild(t);
+    document.body.appendChild(h);
+}
+
